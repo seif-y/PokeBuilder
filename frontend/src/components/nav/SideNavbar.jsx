a33b129bb33eba738d26659104afb6ea8924b7ec
Seif Younes <seif99y@gmail.com>
2021-03-31 00:34:39 +1300

    Added rudimentary navlinks to sidebar

--- frontend/src/components/nav/SideNavbar.jsx
+++ frontend/src/components/nav/SideNavbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "./SideNavbar.module.css";
+import { NavLink } from "react-router-dom";
 
 export default function SideNavbar() {
     return (
         <div className={styles.navbar}>
             <p>Placeholder for now</p>
+            <NavLink to="/pokedex">DEX</NavLink>
+            <NavLink to="/teams">TEAMS</NavLink>
         </div>
     );
 }
