1402546f1abca3cdbe5a676149abe5de4cf98ca4
Sreeniketh Raghavan <sreeniketh0609@gmail.com>
2021-03-30 15:46:51 +1300

    Reformatting all existing files using Prettier

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,15 +4,15 @@ const app = express();
 const port = 3001;
 
 if (process.env.NODE_ENV == "production") {
-  console.log("Running in production environment");
+    console.log("Running in production environment");
 
-  app.use(express.static(path.join(__dirname, "../../frontend/build")));
+    app.use(express.static(path.join(__dirname, "../../frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
-  });
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
+    });
 }
 
 app.listen(port, () => {
-  console.log(`Listening at *:${port}`);
+    console.log(`Listening at *:${port}`);
 });
