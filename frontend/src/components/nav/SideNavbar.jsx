0470490a04a5a22f4d828e230b456e7bd5a3e96b
Seif Younes <seif99y@gmail.com>
2021-03-31 13:52:48 +1300

    Added icons in navbar with placeholder images

--- frontend/src/components/nav/SideNavbar.jsx
+++ frontend/src/components/nav/SideNavbar.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import styles from "./SideNavbar.module.css";
 import { NavLink } from "react-router-dom";
+import NavbarIcon from "./NavbarIcon";
 
 export default function SideNavbar() {
     return (
         <div className={styles.navbar}>
-            <p>Placeholder for now</p>
-            <NavLink to="/pokedex">DEX</NavLink>
-            <NavLink to="/teams">TEAMS</NavLink>
+            <NavLink to="/pokedex">
+                <NavbarIcon imageName="demo.png" />
+            </NavLink>
+            <NavLink to="/teams">
+                <NavbarIcon imageName="demo.png" />
+            </NavLink>
         </div>
     );
 }
