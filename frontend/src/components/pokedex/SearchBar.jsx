1d73c7cf1510ddf87b92f534124e416000115b66
Sreeniketh Raghavan <sreeniketh0609@gmail.com>
2021-04-09 18:08:45 +1200

    Added a SearchBar component for the Pokedex

---
+++ frontend/src/components/pokedex/SearchBar.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+export default function SearchBar({ onSearch }) {
+
+    return (
+        <div>
+            <input type="text"
+                   placeholder="Search"
+                   onKeyUp={e => onSearch(e.target.value)} />
+        </div>
+
+    );
+}
+
+
