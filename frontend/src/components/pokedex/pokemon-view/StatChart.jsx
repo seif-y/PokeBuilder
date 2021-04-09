cb5b2acfe31fd09126522dc20ae0f3008e27c015
Seif Younes <seif99y@gmail.com>
2021-04-09 17:51:34 +1200

    Added better display names for stats

--- frontend/src/components/pokedex/pokemon-view/StatChart.jsx
+++ frontend/src/components/pokedex/pokemon-view/StatChart.jsx
@@ -1,14 +1,29 @@
 import RadarChart from "react-svg-radar-chart";
 import "react-svg-radar-chart/build/css/index.css";
 
+// Object to store full display names for each stat
+const displayNames = {
+    hp: "HP",
+    attack: "Attack",
+    defense: "Defense",
+    specialAttack: "Sp. Atk",
+    specialDefense: "Sp. Def",
+    speed: "Speed",
+};
+
 const MAX_BASE_STAT_VALUE = 255;
 
 export default function StatChart({ stats }) {
+    // Object to hold value of each stat as a portion of the maximum value for a base stat.
+    // Each value will be a number between 0 and 1
     const statPercentages = {};
+
+    // The caption for each stat on the chart will be the display name, and the value for the stat.
     const statCaptions = {};
+
     Object.keys(stats).forEach((statName, index) => {
         statPercentages[statName] = stats[statName] / MAX_BASE_STAT_VALUE;
-        statCaptions[statName] = `${statName} - ${stats[statName]}`;
+        statCaptions[statName] = `${displayNames[statName]} - ${stats[statName]}`;
     });
 
     return <RadarChart captions={statCaptions} data={[{ data: statPercentages }]} />;
