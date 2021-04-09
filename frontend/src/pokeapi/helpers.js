7427432e097296016a8c5a2698ea1155280f33de
Benjamin-Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2021-04-09 22:14:34 +1200

    Added lots of commenting to make helpers.js more clear

--- frontend/src/pokeapi/helpers.js
+++ frontend/src/pokeapi/helpers.js
@@ -1,40 +1,61 @@
 /**
- * FUNCTION REDUCERS
+ * In Mathematics an identity function always returns
+ * the same output as its input.
+ * These identity functions will be used as the initial values for Array.reduce later on
  */
-
-// These functions are initial values for Array.reduce
-// They're handy for when said array only has a length of 1 or 2
-// Identity here means not changing anything
 const fnIdentity = (x) => x;
 const promiseFnIdentity = (x) => Promise.resolve(x);
 
-// Reducing functions, does not handle promise rejections
+/**
+ * Sugar for combining functions into one function
+ * pipe2PromiseFns is the async version
+ * The name pipe is used because these functions work like the Unix pipe "|"
+ */
+const pipe2Fns = (fn1, fn2) => (value) => fn2(fn1(value));
 const pipe2PromiseFns = (promiseFn1, promiseFn2) => (value) => promiseFn1(value).then(promiseFn2);
 
-const pipe2Fns = (fn1, fn2) => (value) => fn2(fn1(value));
-
-// Sugar for reducing functions into one function
-// They're called pipe because they work exactly like the Unix pipe "|"
+/**
+ * pipe2Fns and pipe2PromiseFns take 2 functions and return a single function
+ * This means that they can be used to reduce a list of functions into one function
+ */
 export const pipe = (...fns) => fns.reduce(pipe2Fns, fnIdentity);
 export const pipePromise = (...promiseFns) => promiseFns.reduce(pipe2PromiseFns, promiseFnIdentity);
 
 /**
- * OTHER HELPERS
+ * Most of the below functions are:
+ * - curried, i.e. they keep returning a function until all parameters have been given
+ * - structured so that the important parameters come first
+ *
+ * This means we can easily feed them into pipes and pipePromises
  */
-
 export const filter = (predicate) => (array) => array.filter(predicate);
 
 export const last = (array) => array[array.length - 1];
 
 export const mapPromise = (promiseFn) => (array) => Promise.all(array.map(promiseFn));
 
-export const prop = (key) => (keyable) => keyable[key];
-
-export const propEq = (value) => (key) => (keyable) => keyable[key] === value;
-
 export const replace = (regex, replacement) => (string) => string.replace(regex, replacement);
 
-// (["a", "b"], ["y", "z"]) => ({a: "y", b: "z"})
+/**
+ * - The keys of an array are its indices as well as its length
+ * - The keys of an object are explicitly named
+ */
+
+/**
+ * prop(0)(["first", "second", "third"]) => "first"
+ * prop("first")({first: 1, second: 2, third: 3}) => 1
+ */
+export const prop = (key) => (arrayOrObject) => arrayOrObject[key];
+
+/**
+ * The big brother of prop
+ * we pass in an extra parameter value so we can make a comparison
+ */
+export const propEq = (value) => (key) => (arrayOrObject) => arrayOrObject[key] === value;
+
+/**
+ * (["a", "b"], ["y", "z"]) => ({a: "y", b: "z"})
+ */
 export const zipToObject = (arrayOfKeys, arrayOfValues) =>
     arrayOfKeys.reduce((object, key, index) => {
         object[key] = arrayOfValues[index];
