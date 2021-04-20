ded6f6ebe8aa5e26fe0a428404fe95fcf8acde51
Sreeniketh Raghavan <sreeniketh0609@gmail.com>
2021-04-20 22:16:45 +1200

    Styled the SearchBar component and added a search icon

--- frontend/src/components/pokedex/SearchBar.jsx
+++ frontend/src/components/pokedex/SearchBar.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import styles from "./SearchBar.module.css";
+import SearchIcon from '@material-ui/icons/Search';
+import { Search } from "@material-ui/icons";
 
 export default function SearchBar({ onSearch }) {
 
     return (
-        <div>
+        <div className={styles.barContainer}>
             <input type="text"
                    placeholder="Search"
-                   onKeyUp={e => onSearch(e.target.value)} />
-        </div>
+                   onKeyUp={e => onSearch(e.target.value)}/>
+            <SearchIcon className={styles.searchIcon}
+                        fontSize="1.1rem"
+            />
+            </div>
 
     );
 }
