f8f7f0fe5927975f0f03700e9fafcc9276464576
Benjamin-Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2021-04-26 22:52:08 +1200

    Renders grid of pokemon

--- frontend/src/components/pokedex/Pokedex.jsx
+++ frontend/src/components/pokedex/Pokedex.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import PokemonCard from "./PokemonCardView";
+import { getPokemonViewList } from "../../pokeapi/pokemon";
+
+function GridOfPokemon() {
+    const [pokemonViews, setPokemonViews] = useState([]);
+    useEffect(() => {
+        const updatePokemonViews = async () => {
+            setPokemonViews(await getPokemonViewList());
+        };
+        updatePokemonViews();
+    }, []);
+    return (
+        <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)" }}>
+            {pokemonViews.map(({ id, name, sprite, types }) => (
+                <PokemonCard id={id} name={name} sprite={sprite} types={types} />
+            ))}
+        </div>
+    );
+}
+
 
 export default function Pokedex() {
-    return <div>This is the pokedex</div>;
+    return <GridOfPokemon />;
 }
