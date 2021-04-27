84cf6c6c29ffd653120d455bad649c053d6a42d3
Seif Younes <seif99y@gmail.com>
2021-04-27 14:17:23 +1200

    Minor fix to search bar, used CSS class instead of styling for input component

--- frontend/src/components/pokedex/SearchBar.jsx
+++ frontend/src/components/pokedex/SearchBar.jsx
@@ -5,7 +5,12 @@ import SearchIcon from "@material-ui/icons/Search";
 export default function SearchBar({ onSearch }) {
     return (
         <div className={styles.barContainer}>
-            <input type="text" placeholder="Search" onKeyUp={(e) => onSearch(e.target.value)} />
+            <input
+                className={styles.searchBar}
+                type="text"
+                placeholder="Search"
+                onKeyUp={(e) => onSearch(e.target.value)}
+            />
             <SearchIcon className={styles.searchIcon} fontSize="1.1rem" />
         </div>
     );
