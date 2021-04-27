c2ab2725ce2ba89d37d54c00f5c3dade64242b9f
Seif Younes <seif99y@gmail.com>
2021-04-27 14:55:12 +1200

    Changed pokemon prop to state, and added more styling for TeamMember

--- frontend/src/components/team-builder/TeamMember.jsx
+++ frontend/src/components/team-builder/TeamMember.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import PokeType from "../global/PokeType";
 import styles from "./TeamMember.module.css";
 
-export default function TeamMemberSlot({ pokemon }) {
+export default function TeamMemberSlot() {
+    const [pokemon, setPokemon] = useState(null);
+
     function renderTypes() {
         return pokemon.types.map((type, index) => {
             return <PokeType size="small" typeName={type} onClick={(e) => e.preventDefault()} />;
@@ -10,14 +12,16 @@ export default function TeamMemberSlot({ pokemon }) {
     }
 
     function renderPokemon() {
-        if (pokemon === undefined || pokemon === null) {
+        if (pokemon === null) {
             return "+";
         } else {
             return (
                 <React.Fragment>
-                    <img src={pokemon.sprite} alt="yer" className={styles.pokemonSprite} />
+                    <img src={pokemon.sprite} alt={pokemon.name} className={styles.pokemonSprite} />
                     <div className={styles.pokemonInfo}>
-                        <h3 className={styles.pokemonName}>{pokemon.name}</h3>
+                        <h3 className={styles.pokemonName}>
+                            {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+                        </h3>
                         <div className={styles.typeContainer}>{renderTypes()}</div>
                         <input type="text" placeholder="Notes" />
                     </div>
