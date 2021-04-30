73a8cf02802c4f72ca45e5f481cf6d18d35d4bed
Seif Younes <seif99y@gmail.com>
2021-04-30 13:20:37 +1200

    Added state for team name and description

--- frontend/src/components/team-builder/TeamBuilder.jsx
+++ frontend/src/components/team-builder/TeamBuilder.jsx
@@ -1,12 +1,26 @@
+import { Button } from "@material-ui/core";
 import React, { useState } from "react";
 import TeamMember from "./team-selector/TeamMember";
 import styles from "./TeamBuilder.module.css";
 
 export default function TeamBuilder() {
     const [team, setTeam] = useState(Array(6));
+    const [name, setName] = useState();
+    const [description, setDescription] = useState();
 
     function saveTeam() {
-        console.log(team);
+        // TODO: Once user auth is implemented, set 'creator' to the username of the logged in user
+        let body = {
+            creator: "anonymous",
+            name: name,
+            description: description,
+            party: team,
+        };
+
+        //TODO: Do any required data verification on the frontend.
+
+        //TODO: Make POST request to teams endpoint.
+        console.log(body);
     }
 
     function updateTeam(index, teamMember) {
@@ -29,13 +43,17 @@ export default function TeamBuilder() {
         <div id="modal-root" className={styles.teamBuilderContainer}>
             <div className={styles.titleContainer}>
                 <h1> Team Builder </h1>
-                <button className={styles.saveButton} onClick={() => saveTeam()}>
+                <Button variant="contained" className={styles.saveButton} onClick={() => saveTeam()}>
                     Save
-                </button>
+                </Button>
             </div>
-            <input className={styles.teamNameInput} placeholder="Team Name" />
+            <input className={styles.teamNameInput} placeholder="Team Name" onChange={(e) => setName(e.target.value)} />
             <div className={styles.teamContainer}>{renderTeamSlots()}</div>
-            <textarea className={styles.descriptionInput} placeholder="Team description" />
+            <textarea
+                className={styles.descriptionInput}
+                placeholder="Team description"
+                onChange={(e) => setDescription(e.target.value)}
+            />
         </div>
     );
 }
