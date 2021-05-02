deb2fc946aecb7fbf2a3dbd86a83a306c1b89ebb
Alex Bell <alexrayquaza@gmail.com>
2021-05-02 17:41:33 +1200

    Added dark translucent sidebar styling

--- frontend/src/components/nav/SideNavbar.jsx
+++ frontend/src/components/nav/SideNavbar.jsx
@@ -7,10 +7,19 @@ export default function SideNavbar() {
     return (
         <div className={styles.navbar}>
             <NavLink to="/pokedex" activeClassName={styles.activeNavLink}>
-                <NavbarIcon imageName="demo.png" />
+                <NavbarIcon imageName="pokedex.png" />
             </NavLink>
             <NavLink to="/teams" activeClassName={styles.activeNavLink}>
-                <NavbarIcon imageName="demo.png" />
+                <NavbarIcon imageName="builder.png" />
+            </NavLink>
+            <div className={styles.bottomSpacer}/>
+            <img
+                className={styles.logo}
+                src={`${process.env.PUBLIC_URL}/icons/logo.png`}
+                alt={"Logo"}
+            />
+            <NavLink to="/user" activeClassName={styles.activeNavLink}>
+                <NavbarIcon imageName="user.png" />
             </NavLink>
         </div>
     );
